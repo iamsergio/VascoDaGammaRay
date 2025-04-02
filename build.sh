0dd0665d17e70f48ab12b0b62df157e5ba1369fd
Sergio Martins <sergio.martins@kdab.com>
2025-04-02 12:44:41 +0100

    build: allow to specify VASCO_BUILD_DIR

--- build.sh
+++ build.sh
@@ -5,5 +5,12 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd $SCRIPT_DIR
-cmake --preset rel
-cmake --build build-rel/
+
+BUILD_DIR=build-rel
+if [ -n "$VASCO_BUILD_DIR" ]; then
+    BUILD_DIR=$VASCO_BUILD_DIR
+fi
+
+mkdir -p $BUILD_DIR
+cmake --preset rel -B $BUILD_DIR
+cmake --build $BUILD_DIR
