9ab1a3af6d4a466568ae34109c63a7a7b56c7055
Sergio Martins <sergio.martins@kdab.com>
2025-04-02 16:43:36 +0100

    Add run_example.sh

---
+++ run_example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: MIT
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ ! -d "$1" ]; then
+    echo "Error: First argument is not the build directory."
+    exit 1
+fi
+
+BUILD_DIR=$1
+
+if [ ! -f "$BUILD_DIR/bin/testvasco" ]; then
+    echo "Error: $BUILD_DIR/bin/testvasco does not exist."
+    exit 1
+fi
+
+rm /tmp/testvasco-IpcPipe
+
+LD_PRELOAD=$BUILD_DIR/lib/libvascodagammaray.so $BUILD_DIR/bin/testvasco &
